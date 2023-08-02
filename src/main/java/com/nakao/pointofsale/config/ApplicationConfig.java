9f55ef203c077fd3357d92ac290bb254b07d0317
Naoki Nakao <naokinakao.nn@gmail.com>
2023-08-01 23:17:54 -0400

    Remove bean

--- src/main/java/com/nakao/pointofsale/config/ApplicationConfig.java
+++ src/main/java/com/nakao/pointofsale/config/ApplicationConfig.java
@@ -3,7 +3,6 @@ package com.nakao.pointofsale.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
